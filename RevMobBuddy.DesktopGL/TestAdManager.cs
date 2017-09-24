0aea9c8f078c589d9e0c79538ed2ebedf58d148d
Dan Manning <dmanning23@hotmail.com>
2017-09-23 23:59:38 -0500

    Reorganized files and added desktop test

---
+++ RevMobBuddy.DesktopGL/TestAdManager.cs
@@ -0,0 +1,46 @@
+﻿using RevMobBuddy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReMobBuddy.DesktopGL
+{
+	public class TestAdManager : IAdManager
+	{
+		public event EventHandler OnVideoReward;
+
+		public virtual void DisplayInterstitialAd()
+		{
+		}
+
+		public virtual void DisplayRewardedVideoAd()
+		{
+		}
+
+		public virtual void DisplayVideoAd()
+		{
+		}
+
+		public virtual void HideBannerAd()
+		{
+		}
+
+		public virtual void Initialize(string apiKey)
+		{
+		}
+
+		public virtual void ShowBannerAd()
+		{
+		}
+
+		protected void VideoReward()
+		{
+			if (null != OnVideoReward)
+			{
+				OnVideoReward(this, new EventArgs());
+			}
+		}
+	}
+}
